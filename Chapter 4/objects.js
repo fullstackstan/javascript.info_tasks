3d8323141a600911669de1bd9c93f746bc4f6f08
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-03-28 13:25:03 -0500

    object tasks

--- Chapter 4/objects.js
+++ Chapter 4/objects.js
@@ -56,3 +56,27 @@ alert(sum)
 // Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
 
 // If salaries is empty, then the result must be 0.
+
+
+Multiply numeric property values by 2
+importance: 3
+Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
