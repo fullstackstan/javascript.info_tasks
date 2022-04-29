41d4338ca4f15270a3ca751fb858d50b5bd72d68
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-29 11:12:09 -0500

    paused at filter

--- Chapter 6/Variable scope, closure.js
+++ Chapter 6/Variable scope, closure.js
@@ -75,3 +75,24 @@ alert( counter() ); // 1
 alert( counter2() ); // ?
 alert( counter2() ); // ?
 // solution
+
+
+
+
+
+// Write function sum that works like this: sum(a)(b) = a+b.
+
+// Yes, exactly this way, using double parentheses (not a mistype).
+
+// For instance:
+
+// sum(1)(2) = 3
+//  = 4
+
+function sum(a){
+
+    return function(b){
+       return a+b
+    }
+}
+sum(5)(-1)
