400da1babb986200057bc956e7e61a2d81f6f7ab
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-30 08:56:00 -0500

    tasks

--- Chapter 6/Variable scope, closure.js
+++ Chapter 6/Variable scope, closure.js
@@ -96,3 +96,38 @@ function sum(a){
     }
 }
 sum(5)(-1)
+
+
+
+//////////////////
+
+
+function inBetween(minNumber,maxNumber){
+  return function(x){
+    return x>=minNumber && x<=maxNumber
+
+  }
+}
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+
+function inArray(arr){
+  return function(x){
+    return arr.includes(x)
+  }
+}
+
+
+
+alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
+
+function byField(a,b){
+  return function(){
+    
+  }
+}
+
+
+
