4c7143cf98c03c171ebdf78c43df029b21ec638e
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-06 19:07:17 -0500

    tasks

---
+++ Chapter 6/Function Object, NFE.js
@@ -0,0 +1,13 @@
+function makeCounter() {
+    // instead of:
+    // let count = 0
+  
+    function counter() {
+      return counter.count++;
+    };
+  
+    counter.count = 0;
+  
+    return counter;
+  }
+  
