e6c316756839eb26aacf412f4b2c08891a266a1f
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-13 19:51:07 -0500

    tasks

--- Chapter 6/Decorators.js
+++ Chapter 6/Decorators.js
@@ -4,4 +4,26 @@
 
 // Every call is saved as an array of arguments.
 
-// For instance:
+function spy(func){
+
+    function wrapper(...args){
+        wrapper.calls.push(args)
+        return func.apply(this, args)
+    }
+    wrapper.calls = []
+    return wrapper
+}
+
+
+// Create a decorator delay(f, ms) that delays each call of f by ms milliseconds.
+
+function delay(f, ms){
+
+    return function (){
+        setTimeout(()=>f.apply(this,arguments),ms)
+    }
+   
+
+}
+let f1000= delay(alert,1000)
+f1000('test')
